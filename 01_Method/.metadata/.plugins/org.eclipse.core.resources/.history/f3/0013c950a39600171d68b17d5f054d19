dfa345da94147f355b9369b3a8af128be8624ba8
SteveJones <benkong_ah@foxmail.com>
2017-09-11 14:51:03 +0800

    add

---
+++ 01_Method/.metadata/.plugins/org.eclipse.core.resources/.history/f3/0013c950a39600171d68b17d5f054d19
@@ -0,0 +1,10 @@
+package com.mylib;
+
+public class MyFunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 01_Method/BasicLearning/src/com/mylib/MyArray.java
@@ -0,0 +1,47 @@
+package com.mylib;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+
+public class MyArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 静态初始化
+		int[] ages = new int[]{1,3};
+		System.out.println(ages.length);
+		
+		// 简单写法
+		int[] ages1 = {1, 2, 3};
+		System.out.println(ages1.length);
+		
+		// 动态初始化
+		int[] ages2 = new int[100];
+		System.out.println(ages2.length);
+		
+		
+		// 数组的操作
+//		for (int i = 0; i < ages2.length; i++) {
+//			System.out.println(ages2[i]);
+//		}
+		
+		// 数组的操作异常
+		// 空指针异常 索引越界异常
+		int[] nums = new int[]{10, 11, 20, 30};
+		System.out.println("最大数是=" + MyArray.getMax(nums));
+	}
+	
+	// 获取一个int类型数组中的最大数
+	static int getMax(int[] array){
+		int tmp = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (i < array.length - 1) {
+				int a = array[i];
+				int b = array[i + 1];
+				tmp = a > b ? a : b;
+			}
+		}
+		return tmp;
+	}
+
+}
