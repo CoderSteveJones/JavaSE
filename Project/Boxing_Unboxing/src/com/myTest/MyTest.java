9ef472c45ef6f67a9ce2d53745d46343d5989de1
SteveJones <benkong_ah@foxmail.com>
2017-09-21 10:54:31 +0800

    commit

--- Project/Boxing_Unboxing/src/com/myTest/MyTest.java
+++ Project/Boxing_Unboxing/src/com/myTest/MyTest.java
@@ -4,7 +4,15 @@ public class MyTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// Integer from int 
+		Integer a = 10;
+		// int From Integer
+		int b = a.intValue();
+		// string  from int/Integer
+		String c = "12";
+		// int from str 
+		int d = Integer.parseInt(c);
+		
 	}
 
 }
